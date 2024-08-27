918e6180ae4f583f03fdb59cf43992baa97b95cb
kdohyeon <kdohyeon@gmail.com>
2024-08-28 00:02:49 +0900

    feat: init adapter-http modules

---
+++ netplix-apps/app-api/src/main/java/fast/campus/netplix/NetplixApplication.java
@@ -0,0 +1,11 @@
+package fast.campus.netplix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetplixApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NetplixApplication.class);
+    }
+}
