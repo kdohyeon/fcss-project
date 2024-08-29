ee3ee0e8013ae8afc675c6a7edcf4ca368bef284
kdohyeon <kdohyeon@gmail.com>
2024-08-30 00:20:11 +0900

    feat: added .http and disabled csrf

--- netplix-apps/app-api/src/main/java/fast/campus/netplix/config/SecurityConfig.java
+++ netplix-apps/app-api/src/main/java/fast/campus/netplix/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,7 +14,10 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.authorizeHttpRequests(a -> a.anyRequest().authenticated());
+        httpSecurity.httpBasic(AbstractHttpConfigurer::disable);
+        httpSecurity.csrf(AbstractHttpConfigurer::disable);
+        httpSecurity.authorizeHttpRequests(a -> a.requestMatchers("/api/v1/auth/**").permitAll()
+                .anyRequest().authenticated());
         return httpSecurity.build();
     }
 }
--- netplix-apps/app-api/src/main/java/fast/campus/netplix/controller/auth/AuthController.java
+++ netplix-apps/app-api/src/main/java/fast/campus/netplix/controller/auth/AuthController.java
@@ -8,15 +8,17 @@ import fast.campus.netplix.auth.response.UserRegistrationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
 
     private final RegisterUserUseCase registerUserUseCase;
 
-    @PostMapping("/api/v1/register")
+    @PostMapping("/register")
     public NetplixApiResponse<UserRegistrationResponse> register(
             @RequestBody UserRegistrationRequest request
     ) {
