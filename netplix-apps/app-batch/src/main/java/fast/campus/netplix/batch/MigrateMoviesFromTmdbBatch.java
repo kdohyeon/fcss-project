34ab83b976f422b300e18ce9bff2acac1f2c5d02
kdohyeon <kdohyeon@gmail.com>
2024-09-18 01:13:12 +0900

    feat: movie 조회 배치 chunk 단위로 변경

--- netplix-apps/app-batch/src/main/java/fast/campus/netplix/batch/MigrateMoviesFromTmdbBatch.java
+++ netplix-apps/app-batch/src/main/java/fast/campus/netplix/batch/MigrateMoviesFromTmdbBatch.java
@@ -2,7 +2,8 @@ package fast.campus.netplix.batch;
 
 import fast.campus.netplix.movie.FetchMovieUseCase;
 import fast.campus.netplix.movie.InsertMovieUseCase;
-import fast.campus.netplix.movie.response.MoviePageableResponse;
+import fast.campus.netplix.movie.NetplixMovie;
+import lombok.Builder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
@@ -11,10 +12,13 @@ import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.PlatformTransactionManager;
+
+import java.util.List;
 
 @Slf4j
 @Configuration
@@ -35,20 +39,15 @@ public class MigrateMoviesFromTmdbBatch {
                 .build();
     }
 
+    @Bean(name = "MigrateMoviesFromTmdbBatchTaskletStep")
     public Step step(JobRepository jobRepository, PlatformTransactionManager platformTransactionManager) {
         return new StepBuilder("MigrateMoviesFromTmdbBatchTaskletStep", jobRepository)
-                .tasklet((contribution, chunkContext) -> {
-                    int page = 1;
-                    MoviePageableResponse fetch;
-
-                    do {
-                        fetch = fetchMovieUseCase.fetchFromClient(page);
-                        insertMovieUseCase.insert(fetch.getMovies());
-                        page++;
-                    } while (fetch.getHasNext());
-
-                    return RepeatStatus.FINISHED;
-                }, platformTransactionManager)
+                .chunk(10, platformTransactionManager)
+                .reader(new HttpPageItemReader(1, fetchMovieUseCase))
+                .writer(chunk -> {
+                    List<NetplixMovie> items = (List<NetplixMovie>) chunk.getItems();
+                    insertMovieUseCase.insert(items);
+                })
                 .build();
     }
 }
